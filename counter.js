752fa1f1148f3a1dc7f885bec425cfbb02102604
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-07-30 09:19:38 +0300

    couter updated

--- counter.js
+++ counter.js
@@ -6,10 +6,18 @@ valueDisplays.forEach((valueDisplay) => {
   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
   let duration = Math.floor(interval / endValue);
   let counter = setInterval(function () {
-    startValue +=1;
+   if(endValue < 2000){ startValue +=10;}
+   else if (endValue < 100000){
+        startValue +=50;
+    }
+    else if(endValue > 300000){
+      startValue += 200;
+    if(endValue < startValue){console.log('they are equal')}
+    }
     valueDisplay.textContent = startValue;
     if (startValue == endValue) {
       clearInterval(counter);
+      console.log('true for ' + startValue)
     }
   }, duration);
 });
