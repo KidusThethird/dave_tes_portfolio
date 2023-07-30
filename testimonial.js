59465a522df9451285c051d1e76479bec61160e1
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-07-30 19:45:35 +0300

    updated

--- testimonial.js
+++ testimonial.js
@@ -1,32 +1,16 @@
-
-var testimonials = document.getElementById('testimonials');
-var control1 = document.getElementById('control1');
-var control2 = document.getElementById('control2');
-var control3 = document.getElementById('control3');
-
-
-control1.onclick=function(){
-    testimonials.style.transform = "translateX(870px)";
-    control1.classList.add("active");
-    control2.classList.remove("active");
-    control3.classList.remove("active");
-}
-
-control2.onclick=function(){
-    testimonials.style.transform = "translateX(0px)";
-    control1.classList.remove("active");
-    control2.classList.add("active");
-    control3.classList.remove("active");
-}
-
-control3.onclick=function(){
-    testimonials.style.transform = "translateX(-870px)";
-    control1.classList.remove("active");
-    control2.classList.remove("active");
-    control3.classList.add("active");
-}
-
-
-
-
-
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
